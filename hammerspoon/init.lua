6a810219b4a326ecb388b2d2f79f9745334d6a84
Aybars Hazar Şimşir <aybarshazar@gmail.com>
2019-12-28 15:06:33 +0300

    Add hammerspoon config file

---
+++ hammerspoon/init.lua
@@ -0,0 +1,48 @@
+local hyper = {"ctrl", "alt", "cmd"}
+
+-----------------------------------------------
+-- https://www.hammerspoon.org/Spoons/ReloadConfiguration.html
+-----------------------------------------------
+
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+-----------------------------------------------
+-- Hyper i to show window hints
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, "i", function()
+  hs.hints.windowHints()
+end)
+
+-----------------------------------------------
+-- miro window manager config
+-- https://github.com/miromannino/miro-windows-manager
+-----------------------------------------------
+
+hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "up"},
+  right = {hyper, "right"},
+  down = {hyper, "down"},
+  left = {hyper, "left"},
+  fullscreen = {hyper, "f"}
+})
+
+-----------------------------------------------
+-- Applications
+-- https://github.com/kozko2001/hammerspoon/blob/master/init.lua#L120-L133
+-----------------------------------------------
+
+local apps = {
+  c = 'Google Chrome',
+  a = 'Atom',
+  s = 'Sublime Text',
+  t = 'iTerm'
+}
+
+for key, app in pairs(apps) do
+  hs.hotkey.bind(hyper, key, function() hs.application.launchOrFocus(app) end)
+end
