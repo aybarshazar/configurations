2706c7fdf832ec707667074e07b9994deb5b9f64
Aybars Hazar Şimşir <aybarshazar@gmail.com>
2020-08-23 17:05:02 +0300

    Add a simple focus / break timer to menu bar
    - Displays iteration and break countdowns in the menu bar
    - The timer can be started (or stopped) by clicking the menu bar widget
    - 5 iterations of 6 minutes = 30 minutes
    - Short breaks of 15 seconds between each iteration
    - A long break of 5 minutes at the end of all iterations

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -83,6 +83,124 @@ for key, app in pairs(apps) do
 end
 
 -----------------------------------------------
+-- Focus / break timer
+
+-- Displays iteration and break countdowns in the menu bar
+-- The timer can be started (or stopped) by clicking the menu bar widget
+-- 5 iterations of 6 minutes = 30 minutes
+-- Short breaks of 15 seconds between each iteration
+-- A long break of 5 minutes at the end of all iterations
+-----------------------------------------------
+
+ITERATION_COUNT = 6
+ITERATION_DURATION = 5 * 60 -- 5 minutes
+SHORT_BREAK_DURATION = 15 -- 15 seconds
+LONG_BREAK_DURATION = 5 * 60 -- 5 minutes
+
+local canvas = require("hs.canvas")
+local timer
+local menubar
+local isIdle, isInIteration, isInLongBreak
+local iterationCount, iterationDuration, shortBreakDuration, longBreakDuration
+
+function makeCircleImage(fillColor)
+  local imageHolder = canvas.new({ x = 500, y = 500, h = 16, w = 16 })
+  imageHolder[1] = {
+    action = "fill",
+    fillColor = fillColor,
+    type = "circle",
+  }
+  local circle = imageHolder:imageFromCanvas()
+  imageHolder:delete()
+  return circle
+end
+
+local greenCircle = makeCircleImage({ alpha = 0.7, green = 1.0  })
+local yellowCircle = makeCircleImage({ alpha = 0.7, red = 1, green = 1, blue = 0 })
+local redCircle = makeCircleImage({ alpha = 0.7, red = 1.0 })
+
+function update()
+  if isInLongBreak then
+    local text = os.date("%M:%S", longBreakDuration)
+    menubar:setTitle(text)
+    menubar:setIcon(redCircle, false)
+
+    longBreakDuration = longBreakDuration - 1
+
+    if longBreakDuration == -1 then
+      reset()
+    end
+  elseif isInIteration then
+    local text = os.date("%M:%S", iterationDuration)
+    menubar:setIcon(greenCircle, false)
+    menubar:setTitle(text)
+
+    iterationDuration = iterationDuration - 1
+
+    if iterationDuration == -1 then
+      isInIteration = false
+      iterationDuration = ITERATION_DURATION
+      iterationCount = iterationCount - 1
+
+      if iterationCount == 0 then
+        isInLongBreak = true
+        hs.alert.show("Long break")
+      else
+        hs.alert.show("Short break")
+      end
+    end
+  else
+    local text = os.date("%M:%S", shortBreakDuration)
+    menubar:setTitle(text)
+    menubar:setIcon(yellowCircle, false)
+
+    shortBreakDuration = shortBreakDuration - 1
+
+    if shortBreakDuration == -1 then
+      isInIteration = true
+      shortBreakDuration = SHORT_BREAK_DURATION
+    end
+  end
+end
+
+function start()
+  isIdle = false
+
+  timer:start()
+end
+
+function reset()
+  isIdle = true
+  isInIteration = true
+  isInLongBreak = false
+
+  iterationCount = ITERATION_COUNT
+  iterationDuration = ITERATION_DURATION
+  shortBreakDuration = SHORT_BREAK_DURATION
+  longBreakDuration = LONG_BREAK_DURATION
+
+  timer:stop()
+
+  menubar:setTitle("Start")
+  menubar:setIcon()
+end
+
+function toggle()
+  if isIdle then
+    start()
+  else
+    reset()
+  end
+end
+
+timer = hs.timer.new(1, update)
+
+menubar = hs.menubar.new()
+menubar:setClickCallback(toggle)
+
+reset()
+
+-----------------------------------------------
 -- Debugging / logging utils
 -----------------------------------------------
 
